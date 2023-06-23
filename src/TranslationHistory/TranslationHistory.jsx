0956779e14f8a9f23f9084df20bde27ac303ff5b
Jasim Uddin <jasim.ice.iu@gmail.com>
2023-06-23 22:51:50 +0200

    some components added

---
+++ src/TranslationHistory/TranslationHistory.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TranslationHistory = () => {
+    return (
+        <div>TranslationHistory</div>
+    )
+}
+
+export default TranslationHistory
---
+++ src/components/Navbar/TranslationForm/TranslationForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TranslationForm = () => {
+    return (
+        <div>TranslationForm</div>
+    )
+}
+
+export default TranslationForm
