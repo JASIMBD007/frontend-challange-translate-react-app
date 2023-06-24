2097694bad0cdc55e5fc25f21bfad3ebdaea597d
Jasim Uddin <jasim.ice.iu@gmail.com>
2023-06-24 02:57:44 +0200

    issue with components fixed

--- src/TranslationHistory/TranslationHistory.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const TranslationHistory = () => {
-    return (
-        <div>TranslationHistory</div>
-    )
-}
-
-export default TranslationHistory
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div>About</div>
+        <div>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam obcaecati sed illum ratione ducimus sit laborum recusandae suscipit dolore cum provident sapiente, nihil beatae asperiores omnis repellat animi nam enim? Ea velit, fugiat perspiciatis mollitia numquam rerum est ratione magnam nobis sint magni placeat aperiam, molestias odit possimus facilis recusandae.
+            </p>
+        </div>
     )
 }
 
